3ad8d9d9075ee2338d24e4b203f8f291193136e8
Jonathan Zisser <jonathan@liftforward.com>
2021-02-09 11:03:01 -0500

    Added support for application/csv file MIME types

--- config/initializers/paper_clip.rb
+++ config/initializers/paper_clip.rb
@@ -1,6 +1,6 @@
 require 'paperclip/railtie'
 Paperclip::Railtie.insert
-Paperclip.options[:content_type_mappings] = { csv: 'text/plain' }
+Paperclip.options[:content_type_mappings] = { csv: %w[application/csv text/plain] }
 
 Paperclip.interpolates :timestamp do |attachment, _style|
   attachment.instance_read(:updated_at).strftime('%Y%m%d%H%M%S')
